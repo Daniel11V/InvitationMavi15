6ade7714a18fba830ce925f4bb390464d0344452
Daniel Vinet <daniel1vinet@Gmail.com>
2021-03-18 03:50:04 -0300

    1s Plus Delay and replace some ViewHeight

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -170,3 +170,9 @@ $(function () {
 		}
 	});
 });
+
+////  Variable --vh
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
